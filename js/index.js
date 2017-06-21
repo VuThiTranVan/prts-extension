57872cf227246ec9c9e606236ed1a10d4d443162
Justin <dieunb@gmail.com>
2017-06-21 23:53:40 +0700

    Handle tabs and open pr link

--- js/index.js
+++ js/index.js
@@ -11,45 +11,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         var jsonData = JSON.parse(httpRequest.responseText);
-        // chrome.extension.sendMessage({
-        //   content: jsonData["popup_content"]
-        // });
         var contentWrapper = document.getElementsByClassName('content-wrapper')[0];
         contentWrapper.innerHTML = jsonData['popup_content'];
 
-        var tabs = document.getElementsByClassName('tab');
+        $('#tabs a').click(function (e) {
+          e.preventDefault()
+          $(this).tab('show')
+        })
 
-        for (var i = 0; i < tabs.length; i++) {
-          tabs[i].addEventListener('click', handleTabFunction, false);
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+          $(e.target).parent().addClass('is-active'); // newly activated tab
+          $(e.relatedTarget).parent().removeClass('is-active'); // previous active tab
+        })
+
+        var links = document.querySelectorAll('a.pr');
+
+        for (var i = 0; i < links.length; i++) {
+          (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+              chrome.tabs.create({active: true, url: location});
+            };
+          })();
         }
-        // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        //   chrome.tabs.sendMessage(tabs[0].id, {content: jsonData["popup_content"]}, function(response) {
-        //     // $('#status').html('changed data in page');
-        //     console.log('success' + tabs[0].id);
-        //   });
-        // });
       } else {
         alert('There was a problem with the request.');
       }
-    }
-  }
-
-  var handleTabFunction = function() {
-    var elemId = this.children[0].getAttribute('href');
-    var tabContent = document.getElementById(elemId);
-
-    if(tabContent.className.includes("is-hidden")) {
-      var tabContents = document.getElementsByClassName('tab-content');
-
-      for (var i = 0; i < tabContents.length; i++) {
-        var tab = tabContents[i];
-
-        if (tab != tabContents) {
-          tab.className += 'is-hidden';
-        }
-      }
-
-      tab.className = 'tab-content';
     }
   }
 });
